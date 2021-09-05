48b5a55cce68e1710b78cdd483bbffcecb1c9cae
EdenSheng <singlesaul@163.com>
2021-09-05 20:58:21 +0800

    register koa

---
+++ pages/Register.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login-wrapper">
+    <div class="title">
+      <a-icon class="title-icon" type="dribbble-square" />
+      <span>欢迎注册博客系统</span>
+    </div>
+    <a-form-model :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+      <a-form-model-item label="用户名">
+        <a-input v-model="form.username" placeholder="请输入用户名" />
+      </a-form-model-item>
+      <a-form-model-item label="密码">
+        <a-input-password v-model="form.password" placeholder="请输入密码" />
+      </a-form-model-item>
+      <a-form-model-item label="确认密码">
+        <a-input-password v-model="form.passwordConfirm" placeholder="请输入确认密码" />
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-button type="primary" @click="onSubmit">
+          注册
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue{
+  form: FormInterface = {
+    name: '',
+    password: '',
+    passwordConfirm: ''
+  }
+  labelCol: SpaceColInterface = { span: 6 }
+  wrapperCol: SpaceColInterface = { span: 18 }
+
+  onSubmit(){
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.login-wrapper{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .title{
+    font-family: PingFangSC-Medium, sans-serif;
+    font-size: 24px;
+    color: #333;
+    padding-bottom: 24px;
+
+    .title-icon{
+      font-size: 24px;
+    }
+  }
+}
+/deep/ .ant-form{
+  width: 400px;
+  border: 2px solid #333;
+  padding: 24px 24px 0 24px;
+  border-radius: 10px;
+}
+</style>
--- types/custom.d.ts
+++ types/custom.d.ts
@@ -13,3 +13,92 @@ type Result = {
   message: string;
   status: boolean;
 }
+
+/* TreeSelectDate Interface */
+
+interface TreeSelectDataItem {
+  title: string,
+  value: string | number,
+  key: string | number,
+  selectable?: boolean,
+  children?: TreeSelectDataItem[]
+}
+
+interface TreeSelectDataInterface extends Array<TreeSelectDataItem> {
+}
+
+/* TreeDate Interface */
+
+interface TreeDataItem {
+  title: string,
+  num: string | number,
+  key: string,
+  children?: TreeDataItem[]
+}
+
+interface TreeDataInterface extends Array<Array<TreeDataItem>> {
+}
+
+/* Form Interface */
+
+type FormInterface = {
+  [key: string]: any
+}
+
+/* SpaceCol Interface */
+
+interface SpaceColInterface {
+  xs?: { span: number },
+  sm?: { span: number },
+  md?: { span: number },
+  lg?: { span: number },
+  xl?: { span: number },
+  xxl?: { span: number },
+  span?: number
+}
+
+/* Columns Interface */
+
+interface ColumnItem {
+  title: string
+  align?: string
+  dataIndex: string
+  key?: string
+  ellipsis?: true
+  width: number
+  filter?: any
+  disabled?: boolean
+  scopedSlots?: { customRender: string }
+  customRender?: Function
+}
+
+interface ColumnsInterface extends Array<ColumnItem> {
+
+}
+
+/* Select Interface */
+
+interface SelectItem {
+  value: string | number
+  label: string
+  title?: string
+  disabled?: boolean
+}
+
+interface SelectInterface extends Array<SelectItem> {
+
+}
+
+/* Pagination Interface */
+
+interface PaginationInterface {
+  current: number | undefined
+  pageNum?: number
+  pageSize: number
+  pageSizeOptions: Array<string>
+  showTotal(total: number, range: Array<any>): string
+  itemRender(current: number, type: string, originalElement: HTMLElement): HTMLElement
+  showQuickJumper: boolean
+  showSizeChanger: boolean
+  total: number
+}
